62fa06108d5cc1d016aeeb9f5d63b40fe8e62cb1
mertunctuncer <mertunctuncer@gmail.com>
2024-01-28 00:30:39 +0300

    Unit tests for Range

--- src/test/java/me/mertunctuncer/peorm/api/util/RangeTest.java
+++ src/test/java/me/mertunctuncer/peorm/api/util/RangeTest.java
@@ -8,15 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RangeTest {
 
-
-    // returns true if value is within the range
     @Test
     public void contains_ValueWithinRange() {
         Range range = new Range(1, 10);
         assertTrue(range.contains(5));
     }
 
-    // returns true if value is equal to start or end of the range
     @Test
     public void contains_ValueEqualToStartOrEnd() {
         Range range = new Range(1, 10);
@@ -24,14 +21,12 @@ public class RangeTest {
         assertTrue(range.contains(10));
     }
 
-    // returns false if value is outside the range
     @Test
     public void contains_ValueOutsideRange() {
         Range range = new Range(1, 10);
         assertFalse(range.contains(15));
     }
 
-    // returns true if value is equal to start or end of the range and range is empty
     @Test
     public void contains_ValueEqualToStartOrEndAndEmptyRange() {
         Range range = new Range(5, 5);
