dda5ecab84e413890c42aea142d0793a5653017f
mertunctuncer <mertunctuncer@gmail.com>
2024-07-16 12:21:26 +0300

    Added tests

--- src/test/java/me/mertunctuncer/peorm/internal/reflection/ClassReaderTest.java
+++ src/test/java/me/mertunctuncer/peorm/internal/reflection/ClassReaderTest.java
@@ -1,8 +1,13 @@
 package me.mertunctuncer.peorm.internal.reflection;
 
+import me.mertunctuncer.peorm.api.util.Pair;
+import me.mertunctuncer.peorm.internal.reflection.model.ColumnData;
 import me.mertunctuncer.peorm.mock.NamedMockTable;
 import me.mertunctuncer.peorm.mock.NamelessMockTable;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +23,34 @@ class ClassReaderTest {
     void table_name_should_be_read_correctly() {
         assertEquals(ClassReader.getTableName(NamedMockTable.class), "named-mock-table");
         assertEquals(ClassReader.getTableName(NamelessMockTable.class), "namelessmocktable");
+    }
+
+    @Test
+    void fields_should_be_read_correctly() {
+        List<Pair<Field, ColumnData>> fieldData = ClassReader.mapFields(NamedMockTable.class);
+
+        assertEquals(3, fieldData.size());
+
+        ColumnData id = fieldData.get(0).second();
+        ColumnData count = fieldData.get(1).second();
+        ColumnData name = fieldData.get(2).second();
+
+        assertEquals(id.name(), "no");
+        assertTrue(id.primaryKey());
+        assertTrue(id.identity());
+        assertTrue(id.nullable());
+        assertEquals(id.identitySeed(), 9);
+        assertEquals(id.identityIncrementAmount(), 2);
+
+        assertEquals(count.name(), "count");
+        assertTrue(count.autoIncrement());
+        assertEquals(count.autoIncrementAmount(), 2);
+        assertFalse(count.nullable());
+
+        assertEquals(name.name(), "NAME");
+        assertTrue(name.foreignKey());
+        assertFalse(name.nullable());
+        assertEquals(name.size(), 123);
     }
 
 }
--- src/test/java/me/mertunctuncer/peorm/mock/NamedMockTable.java
+++ src/test/java/me/mertunctuncer/peorm/mock/NamedMockTable.java
@@ -5,12 +5,18 @@ import me.mertunctuncer.peorm.api.annotation.*;
 @Table(name = "named-mock-table")
 public class NamedMockTable {
     @Column(name = "no")
-    @Identity @NotNull
+    @PrimaryKey @Identity(seed = 9, increment = 2)
     private int id;
 
     @Column
     @AutoIncrement ( increment = 2 ) @NotNull
     private int count;
+
+
+    @Column(name = "NAME")
+    @ForeignKey @NotNull @Size(size = 123) @Unique
+    private String name;
+
 
 
 
