f0a3f2cc7a969723ef340825a6f93798dd412103
Aki <mertunctuncer@gmail.com>
2024-08-02 21:45:37 +0300

    Implemented UpsertQuery

--- src/main/java/me/mertunctuncer/peorm/query/UpsertQuery.java
+++ src/main/java/me/mertunctuncer/peorm/query/UpsertQuery.java
@@ -1,50 +1,71 @@
 package me.mertunctuncer.peorm.query;
 
 
+import me.mertunctuncer.peorm.model.ColumnProperties;
 import me.mertunctuncer.peorm.model.ReflectionContainer;
 import me.mertunctuncer.peorm.model.TableProperties;
-import me.mertunctuncer.peorm.util.IndexedSQLMap;
+import me.mertunctuncer.peorm.util.SQLPairList;
 
 public final class UpsertQuery<T> implements Query<T> {
 
     private final TableProperties<T> tableProperties;
-    private final IndexedSQLMap rowData;
+    private final SQLPairList whereConstraints;
+    private final SQLPairList newValues;
 
-    private UpsertQuery(TableProperties<T> tableProperties, IndexedSQLMap rowData) {
+    private UpsertQuery(TableProperties<T> tableProperties, SQLPairList whereConstraints, SQLPairList newValues) {
         this.tableProperties = tableProperties;
-        this.rowData = rowData;
+        this.whereConstraints = whereConstraints;
+        this.newValues = newValues;
     }
 
     @Override
-    public TableProperties<T> getTableData() {
+    public TableProperties<T> getTableProperties() {
         return tableProperties;
     }
 
-    public IndexedSQLMap getRowData() {
-        return rowData;
+    public SQLPairList getWhereConstraints() {
+        return whereConstraints;
     }
 
-    public static final class Builder<T> {
+    public SQLPairList getNewValues() {
+        return newValues;
+    }
 
-        public final TableProperties<T> tableProperties;
-        public IndexedSQLMap values;
+    public static final class Builder<T> implements QueryBuilder<T>{
 
-        public Builder(TableProperties<T> tableProperties) {
+        private TableProperties<T> tableProperties;
+        private SQLPairList whereConstraints;
+        private SQLPairList newValues;
+
+
+        @Override
+        public Builder<T> withTableProperties(TableProperties<T> tableProperties) {
             this.tableProperties = tableProperties;
+            return null;
         }
 
-        public UpsertQuery.Builder<T> rowData(T rowData, ReflectionContainer<T> reflectionContainer) {
-            this.values = IndexedSQLMap.Factory.create(rowData, tableProperties, reflectionContainer);
+        public Builder<T> withWhereConstraintsByPrivateKey(T primaryKeyOwner, ReflectionContainer<T> reflectionContainer) {
+            this.whereConstraints = SQLPairList.Factory.create(primaryKeyOwner, tableProperties, reflectionContainer, ColumnProperties::primaryKey);
             return this;
         }
 
-        public UpsertQuery.Builder<T> rowData(IndexedSQLMap rowData) {
-            this.values = rowData;
+        public Builder<T> withWhereConstraints(SQLPairList whereConstraints) {
+            this.whereConstraints = whereConstraints;
+            return this;
+        }
+
+        public Builder<T> withNewValuesFromInstance(T instance, ReflectionContainer<T> reflectionContainer) {
+            this.newValues = SQLPairList.Factory.create(instance, tableProperties, reflectionContainer);
+            return this;
+        }
+
+        public Builder<T> withNewValues(SQLPairList newValues) {
+            this.newValues = newValues;
             return this;
         }
 
         public UpsertQuery<T> build() {
-            return new UpsertQuery<>(tableProperties, values);
+            return new UpsertQuery<>(tableProperties, whereConstraints, newValues);
         }
     }
 }
